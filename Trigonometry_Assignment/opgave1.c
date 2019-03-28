ffa71ce9756d2733c6827753144880f6daf9c1a5
TamLe2001 <TamLe2001@live.dk>
2019-03-28 15:22:27 +0100

    asdslaskdasodp

--- Trigonometry_Assignment/opgave1.c
+++ Trigonometry_Assignment/opgave1.c
@@ -1,1 +1,17 @@
 #include "Prototype.h"
+
+void calculateArea()
+{
+    float a, b, c, s, area;
+    printf("You chose to calculate the area of an arbitrary triangle.\n");
+    printf("Enter side a: \n");
+    scanf("%",&a);
+    printf("Enter side b: \n");
+    scanf("%f",&b);
+    printf("Enter side c: \n");
+    scanf("%f",&c);
+    // Herons formel: sqrt(s*(s-a)*(s-b)*(s-c) hvor s = (a+b+c)/2
+    s = (a+b+c)/2;
+    area = sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("The area of the triangle is: %f", area);
+}
