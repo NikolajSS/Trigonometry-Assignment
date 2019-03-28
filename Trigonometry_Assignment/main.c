89f870aa8fc56b9b9152b44bef549e046c71fa4a
NikolajSS <NikolajSS1907@gmail.com>
2019-03-28 14:13:31 +0100

    Update main.c

--- Trigonometry_Assignment/main.c
+++ Trigonometry_Assignment/main.c
@@ -15,9 +15,12 @@ switch (option)
 
 void calculateArea()
 {
+    float a, b, c
     printf("You chose to calculate the area of an arbitrary triangle.\n");
     printf("Enter side a: \n");
-    scanf("%")
+    scanf("%",&a);
     printf("Enter side b: \n");
+    scanf("%f",&b);
     printf("Enter side c: \n");
+    scanf("%f",&c);
 }
