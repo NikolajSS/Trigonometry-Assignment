525c4fcb697919005a457de5bf7b0a88e90be224
TamLe2001 <TamLe2001@live.dk>
2019-03-28 18:25:20 +0100

    Opgave 2 fixed done I guess

--- Trigonometry_Assignment/opgave2.h
+++ Trigonometry_Assignment/opgave2.h
@@ -13,6 +13,10 @@ void opgave2()
     printf("Enter side c: \n");
     scanf("%f",&c);
     // Herons formel: sqrt(s*(s-a)*(s-b)*(s-c) hvor s = (a+b+c)/2
+    angelA = acos(((pow(b,2)+pow(c,2)-pow(a,2))/(2*b*c)))*180/PI;
+    angelB = acos(((pow(a,2)+pow(c,2)-pow(b,2))/(2*a*c)))*180/PI;
     angelC = acos(((pow(a,2)+pow(b,2)-pow(c,2))/(2*a*b)))*180/PI;
+    printf("The area of the triangle is: %.2f\n", angelA);
+    printf("The area of the triangle is: %.2f\n", angelB);
     printf("The area of the triangle is: %.2f\n", angelC);
 }
--- Trigonometry_Assignment/opgave3.h
+++ Trigonometry_Assignment/opgave3.h
@@ -1,5 +1,16 @@
 #include "Bio.h"
 
-void opgave3(){
-
+void opgave3()
+{
+    float a, b, c, angelC;
+    printf("You chose to calculate the area of an arbitrary triangle.\n");
+    printf("Enter side a: \n");
+    scanf("%f",&a);
+    printf("Enter side b: \n");
+    scanf("%f",&b);
+    printf("Enter side c: \n");
+    scanf("%f",&c);
+    // Herons formel: sqrt(s*(s-a)*(s-b)*(s-c) hvor s = (a+b+c)/2
+    angelC = acos(((pow(a,2)+pow(b,2)-pow(c,2))/(2*a*b)))*180/PI;
+    printf("The area of the triangle is: %.2f\n", angelC);
 }
