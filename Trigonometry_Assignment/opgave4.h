f05552542914520314e8e605ba1f3f3d11151c07
TamLe2001 <TamLe2001@live.dk>
2019-03-29 09:50:15 +0100

    Opgave 4 fixed

--- Trigonometry_Assignment/opgave4.h
+++ Trigonometry_Assignment/opgave4.h
@@ -3,8 +3,9 @@
 void opgave4()
 {
     float c1=30, b1=14, C1=110, B1;
-    float c2, b2, C2, B2, val;
+    float c2, b2, C2, B2, val, val2;
     val = 180 / PI;
+    val2 = PI / 180;
 
     printf("You chose to calculate the area of an arbitrary triangle.\n");
     // Herons formel: sqrt(s*(s-a)*(s-b)*(s-c) hvor s = (a+b+c)/2
@@ -22,7 +23,7 @@ void opgave4()
     scanf("%f",&C2);
     // Herons formel: sqrt(s*(s-a)*(s-b)*(s-c) hvor s = (a+b+c)/2
 
-    B2 = asin((sin(C2*PI/180)*(b2)/(c2)))*val;
+    B2 = asin((sin(C2*val2)*(b2)/(c2)))*val;
     printf("The area of the triangle is: %.2f\n", B2);
 }
 
