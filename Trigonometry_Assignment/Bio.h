ecf2451909add7c0b7b550694e5ef7d839055411
TamLe2001 <TamLe2001@live.dk>
2019-03-30 22:50:37 +0100

    l

--- Trigonometry_Assignment/Bio.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <windows.h>
-#define PI 3.14159265358979323846
--- Trigonometry_Assignment/Opgaverne.h
+++
@@ -1,6 +0,0 @@
-#include "opgave1.h"
-#include "opgave2.h"
-#include "opgave3.h"
-#include "opgave4.h"
-#include "opgave5.h"
-
