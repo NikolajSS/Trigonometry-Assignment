4b813678d047ba925ff91f60aa19c790dbb931d3
TamLe2001 <TamLe2001@live.dk>
2019-04-01 08:51:44 +0200

    Process 6

--- Trigonometry_Assignment/opgave6.h
+++ Trigonometry_Assignment/opgave6.h
@@ -3,7 +3,7 @@
 
 void opgave6()
 {
-    float vinkel1, vinkel2, side, rside1, rside2;
+    float vinkel1, vinkel2, vinkel3, side, rside1, rside2;
 
     printf("You chose to calculate the area of an arbitrary triangle.\n");
     printf("Enter side c: \n");
@@ -14,6 +14,8 @@ void opgave6()
     scanf("%f",&side);
     // Herons formel: sqrt(s*(s-a)*(s-b)*(s-c) hvor s = (a+b+c)/2
 
+    vinkel3= 180-vinkel1-vinkel2;
+    angelC = acos(((pow(a,2)+pow(b,2)-pow(c,2))/(2*a*b)))*180/PI;
     B2 = asin((sin(C2*val2)*(b2)/(c2)))*val;
     printf("The area of the triangle is: %.2f\n", B2);
 }
