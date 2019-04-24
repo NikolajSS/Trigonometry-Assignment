b2779bf63a01f1acebe621e64a51d9e8fffd3916
NikolajSS <NikolajSS1907@gmail.com>
2019-04-24 21:18:31 +0200

    til kenny

--- Trigonometry_Assignment/Bio.h
+++ Trigonometry_Assignment/Bio.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <windows.h>
 #define PI 3.14159265358979323846
-#define val 180 / PI
-#define val2 PI / 180
+#define val 180 / PI //Helps us converting RADIANS into DEGREES
+#define val2 PI / 180 //Helps us converting DEGREES into RADIANS
 
 ///Libraries get included and values get defined
