b83b2a334d25d03e7da11e242bca9ed8dd95a913
TamLe2001 <TamLe2001@live.dk>
2019-04-24 14:42:50 +0200

    Error 6

--- Trigonometry_Assignment/opgave6.h
+++ Trigonometry_Assignment/opgave6.h
@@ -3,24 +3,26 @@
 
 void opgave6()
 {
-    float vinkel1, vinkel2, vinkel3, side, rside1, rside2;
+    float vinkel1, vinkel2, rvinkel3, side, rside1, rside2;
 
     printf("You chose to calculate the area of an arbitrary triangle.\n");
-    printf("Enter side c: \n");
+    printf("Enter angel: \n");
     scanf("%f",&vinkel1);
-    printf("Enter side b: \n");
+    printf("Enter angel: \n");
     scanf("%f",&vinkel2);
-    printf("Enter angle C: \n");
+    printf("Enter side: \n");
     scanf("%f",&side);
     // Herons formel: sqrt(s*(s-a)*(s-b)*(s-c) hvor s = (a+b+c)/2
 
 
-    vinkel3= 180-vinkel1-vinkel2;
-    rside1 = (sin(vinkel2)*val)*(side)/(sin(vinkel1)*val);
-    rside2 = asin((sin(C2*val2)*(b2)/(c2)))*val;
+    rvinkel3= 180-vinkel1-vinkel2;
+    rside1 = (sin(vinkel2)*val2)*((side)/(sin(vinkel1)*val));
+   // rside2 = asin((sin(C2*val2)*(b2)/(c2)))*val;
+    rside2 = (sin(rvinkel3)*val2)*((rside1)/(sin(vinkel2)*val));
 
     printf("The side of the triangle is: %.2f\n", rside1);
     printf("The side of the triangle is: %.2f\n", rside2);
+    printf("The angle of the triangle is: %.2f\n", rvinkel3);
     next();
 }
 
